5b2f36d398eea6d227510c41e37123c2813642bf
Zhang12334 <3143448821@qq.com>
2024-11-16 16:41:17 +0800

    fix bug

--- src/main/java/com/meow/meowvelolist/Metrics.java
+++ src/main/java/com/meow/meowvelolist/Metrics.java
@@ -12,7 +12,7 @@
  *
  * Violations will result in a ban of your plugin and account from bStats.
  */
-package org.bstats.velocity;
+package com.meow.meowvelolist;
 
 import com.google.inject.Inject;
 import com.velocitypowered.api.plugin.PluginContainer;
